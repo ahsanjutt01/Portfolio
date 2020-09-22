445687ccc513a79c072e194d3742c6a3cd4d3a6a
M. Ahsan Jutt <ahsanjutt0999@outlook.com>
2020-09-23 03:57:12 +0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,39 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
 
+import { NgModule } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SkillComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    CollapseModule.forRoot(),
+    NgxNavbarModule,
+    // AngularFontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/skill/skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.css']
+})
+export class SkillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
